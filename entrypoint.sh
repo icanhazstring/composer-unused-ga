02a00a0a659bff3f9d9532a37d016e10f80ecab9
Andreas Frömer <icanhazstring@users.noreply.github.com>
2019-10-05 12:59:24 +0200

    Change entrypoint command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
 
-sh -c "composer global require icanhazstring/composer-unused"
-sh -c "composer install --no-scripts --no-progress"
-sh -c "composer unused"
+sh -c "composer global require icanhazstring/composer-unused && composer install --no-scripts --no-progress && sh -c composer unused"
